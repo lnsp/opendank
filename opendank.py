cca75a33c7fa024f385bb5b2d5c4b7cd1d682b76
Lennart Espe <espe@at-1-9-8.stud.rbg.tum.de>
2016-10-04 15:25:12 +0200

    Add image fetching

--- opendank.py
+++ opendank.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python2
-
 import praw
-r = praw.Reddit(user_agent='opendank')
-submissions = r.get_subreddit('pics').get_hot(limit=5)
-for x in submissions:
-    print(x)
+import requests
+from PIL import Image
+
+def fetch_images():
+    r = praw.Reddit(user_agent='funny')
+    submissions = r.get_subreddit('pics').get_hot(limit=20)
+    imageId = 0
+    for x in submissions:
+        if "i.imgur.com/" in x.url:
+            response = requests.get(x.url)
+            if response.status_code == 200:
+                with open("image" + str(imageId), 'wb') as img:
+                    for chunk in response.iter_content(4096):
+                        img.write(chunk)
+                imageId += 1
+    return imageId
+
+fetch_images()
+
