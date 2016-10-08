c26abc75c9b8b025adba5611134fa821919fc273
Lennart Espe <lennart@mooxmirror.io>
2016-10-08 18:34:56 +0200

    Move CustomSource into sources module

---
+++ opendank.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+import diashow
+import sources
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Missing configuration argument'
+        return
+
+    dia = diashow.Diashow()
+    with file(sys.argv[1]) as f:
+        for line in f:
+            dia.add_source(sources.CustomSource(line))
+    dia.start()
+
+if __name__ == "__main__":
+    main()
--- sources.py
+++ sources.py
@@ -62,3 +62,31 @@ class Sysadminotaur(HtmlSource):
             if content.get('name') == 'main':
                 urls.append('http://sysadminotaur.com/' + content.get('src'))
         return urls
+
+class CustomSource(HtmlSource):
+    def fetch_source(self, selection, tokens):
+        results = []
+        if tokens[0] == ">":
+            for element in selection.find_all(tokens[1]):
+                results += self.fetch_source(element, tokens[2:])
+        elif tokens[0] == ":":
+            for element in selection.find_all(attrs={tokens[1]: tokens[3]}):
+                results += self.fetch_source(element, tokens[4:])
+        elif tokens[0] == "!":
+            value = selection[tokens[1]]
+            tokens = tokens[2:]
+            while len(tokens) > 0:
+                if tokens[0] == "prefix":
+                    value = tokens[1] + value
+                elif tokens[0] == "suffix":
+                    value = value + tokens[1]
+                tokens = tokens[2:]
+            results += [value]
+        return results
+
+    def __init__(self, pattern):
+        self.tokens = pattern.split(' ')
+
+    def fetch_images(self):
+        soup = self.get_soup(self.tokens[0])
+        return self.fetch_source(soup, self.tokens[1:])
