80777adde558bd0df763080355aee2a152e0fa07
Lennart Espe <lennart@mooxmirror.io>
2016-10-08 13:50:40 +0200

    Add docstrings to diashow module

--- diashow.py
+++ diashow.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python2
+"""Module diashow provides functionality for displaying images from the WWW."""
 import sys
 import os
 import glob
 import datetime
-import praw
-import requests
 import Tkinter as tk
-import time
+import requests
 from PIL import ImageTk, Image
 
 
-class Diashow:
+class Diashow(object):
+    """Class Diashow downloads and displays images."""
+
+    # pylint: disable=too-many-instance-attributes
 
     def __init__(
             self,
@@ -23,6 +24,7 @@ class Diashow:
         self.sources = []
         self.image_prefix = image_prefix
         self.last_fetch_date = datetime.date.fromtimestamp(0)
+        self.images = []
 
         self.window = tk.Tk()
         self.window.attributes('-fullscreen', True)
@@ -34,6 +36,7 @@ class Diashow:
         self.panel.pack(side='bottom', fill='both', expand='yes')
 
     def store_image(self, url, filename):
+        """Downloads an image from a specific url and stores it."""
         response = requests.get(url)
         if response.status_code == 200:
             with open(filename, 'wb') as image:
@@ -43,9 +46,11 @@ class Diashow:
         return False
 
     def add_source(self, source_class):
+        """Add image source to diashow."""
         self.sources.append(source_class)
 
     def fetch_images(self):
+        """Fetch images from sources."""
         self.images = []
         image_urls = []
         for source in self.sources:
@@ -61,13 +66,15 @@ class Diashow:
             if self.store_image(url, image_path):
                 self.images.append(image_path)
                 current_image += 1
-        print(']')
+        print ']'
 
     def clear_cache(self):
+        """Remove stored image artifacts."""
         for filename in glob.glob(self.image_prefix + '*'):
             os.remove(filename)
 
     def display_active(self):
+        """Update screen to show active image."""
         photo = Image.open(self.images[self.active])
         target_size = (self.width, self.height)
         photo.thumbnail(target_size, Image.ANTIALIAS)
@@ -76,6 +83,7 @@ class Diashow:
         self.panel.image = image
 
     def update(self):
+        """Update the screen, images and sources."""
         current_date = datetime.date.today()
         if current_date.day != self.last_fetch_date.day:
             self.clear_cache()
@@ -85,12 +93,14 @@ class Diashow:
         self.display_active()
         self.window.after(self.update_interval, self.update)
 
-    def destroy(self, event):
+    def destroy(self, _):
+        """Destroy the diashow window and free all resources."""
         self.window.destroy()
         self.clear_cache()
         sys.exit(0)
 
     def start(self):
+        """Start the diashow."""
         self.update()
         self.window.bind(self.quit_key, self.destroy)
         self.window.mainloop()
