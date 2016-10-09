d4acd1b2b217461f0a346c47f35a082208410d1f
Lennart Espe <lennart@mooxmirror.io>
2016-10-09 20:13:34 +0200

    Add more metadata

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ setup(
     version="0.1",
     description="View images from the WWW in a diashow.",
     url="https://github.com/lnsp/opendank",
+    download_url="https://github.com/lnsp/opendank/tarball/0.1",
     author="the opendank community",
+    author_email="lennart@mooxmirror.io",
     license="MIT",
     packages=["opendank"],
+    keywords=["memes", "www", "diashow"],
+    classifiers=[],
     install_requires=[
         "praw",
         "requests",
